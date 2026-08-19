1fab21b385751299b4e6109afce2fcd630661b39
Shivam Kumar Soni <shivam.soni.aiml@gmail.com>
2026-08-19 22:18:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Identical Linked Lists/identical-linked-lists.cpp
@@ -0,0 +1,30 @@
+/* Strucutre of a Node in linked list
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+}; */
+class Solution {
+  public:
+    bool areIdentical(Node *head1, Node *head2) {
+        // code here
+        Node * temp1 = head1 ;
+        Node * temp2 = head2;
+        
+        while (temp1 != NULL && temp2 != NULL) {
+            if (temp1->data != temp2->data) {
+                return false;
+            }
+
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+
+        return temp1 == NULL && temp2 == NULL;
+    }
+};
