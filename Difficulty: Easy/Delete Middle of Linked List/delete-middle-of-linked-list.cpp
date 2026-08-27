995b5b2ee20dde1fd79aa03b69b6744467fc6804
Shivam Kumar Soni <shivam.soni.aiml@gmail.com>
2026-08-27 18:49:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Middle of Linked List/delete-middle-of-linked-list.cpp
@@ -0,0 +1,39 @@
+/* Node Structure
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+}; */
+
+class Solution {
+
+  public:
+    Node* deleteMid(Node* head) {
+        // code here
+        if(head == NULL || head -> next == NULL){
+            return NULL;
+        }
+        
+
+        Node * slow = head;
+        Node * fast = head;
+        Node* prev = NULL;
+
+   
+
+        while (fast != NULL && fast->next != NULL) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        prev -> next = slow -> next ;
+        slow -> next = NULL;
+        delete slow;
+        
+        return head;
+    }
+};
