c6f64d532b6d362c417f757c413bda6b71b229ea
Shivam Kumar Soni <shivam.soni.aiml@gmail.com>
2026-09-09 14:06:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Implement Stack using Array/implement-stack-using-array.cpp
@@ -0,0 +1,55 @@
+class myStack {
+    private:
+        int *arr;
+        int top ;
+        int size;
+  public:
+    myStack(int n) {
+        // Define Data Structures
+        this -> size = n ;
+        arr = new int [size];
+        top = -1 ;
+        
+    }
+
+    bool isEmpty() {
+        // check if the stack is empty
+        if (top == -1 ){
+            return true;
+        }
+        return false ;
+    }
+
+    bool isFull() {
+        // check if the stack is full
+        if (top == size-1){
+            return true ;
+        }
+        return false;
+    }
+
+    void push(int x) {
+        // inserts x at the top of the stack
+        if (!isFull()){
+            top++;
+            arr[top] = x;
+        }
+    
+    }
+
+    void pop() {
+        // removes an element from the top of the stack
+        if (isEmpty()){
+            return  ;
+        }
+        top --;
+    }
+
+    int peek() {
+        // Returns the top element of the stack
+        if (isEmpty()){
+            return -1 ;
+        }
+        return arr[top];
+    }
+};
