6ab8684c5693ecd30308bebb718350b4c0724cea
Shivam Kumar Soni <shivam.soni.aiml@gmail.com>
2026-09-23 10:58:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Inorder Traversal/inorder-traversal.cpp
@@ -0,0 +1,37 @@
+/* Structure of Binary Tree Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};*/
+
+class Solution {
+    private:
+        void solve(Node* root, vector<int>& ans) {
+            if (root == NULL) {
+                return;
+            }
+
+        solve(root->left, ans);
+
+        ans.push_back(root->data);
+
+        solve(root->right, ans);
+    }
+  public:
+    vector<int> inOrder(Node* root) {
+        // code here
+        vector<int> ans;
+
+        solve(root, ans);
+
+        
+    return ans ;
+    }
+};
