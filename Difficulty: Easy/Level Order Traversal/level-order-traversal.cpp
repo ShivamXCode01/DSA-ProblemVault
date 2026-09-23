3dd0c9ad23a1681aaf92561f44b8498adf3c04fd
Shivam Kumar Soni <shivam.soni.aiml@gmail.com>
2026-09-23 12:14:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Level Order Traversal/level-order-traversal.cpp
@@ -0,0 +1,45 @@
+/* Structure of Binary Tree Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};*/
+
+class Solution {
+  public:
+    vector<int> levelOrder(Node *root) {
+        // code here
+    vector<int> ans;
+
+       if (root == NULL) {
+           return ans;
+       }
+
+       queue<Node*> q;
+       q.push(root);
+
+       while (!q.empty()) {
+
+        Node* temp = q.front();
+        q.pop();
+
+        ans.push_back(temp->data);
+
+        if (temp->left) {
+            q.push(temp->left);
+        }
+
+        if (temp->right) {
+            q.push(temp->right);
+        }
+    }
+       return ans;        
+    }
+};
