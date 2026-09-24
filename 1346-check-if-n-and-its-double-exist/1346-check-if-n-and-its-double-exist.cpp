826239c021136a1d8aea5c64918265c959331e64
Shivam Kumar Soni <shivam.soni.aiml@gmail.com>
2026-09-24 19:20:26 +0530

    Time: 6 ms (6.77%), Space: 14 MB (17.1%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        set<int>s;
+        for (int n : arr){
+             if (s.contains(n * 2) || 
+                (n % 2 == 0 && s.contains(n / 2))){
+                return true;
+            }
+            s.insert(n);
+        }
+        return false;
+    }
+};
