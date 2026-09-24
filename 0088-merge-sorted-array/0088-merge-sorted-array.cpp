b70e643b1c8a4a030a53f2f319c2f8a1cb05141d
Shivam Kumar Soni <shivam.soni.aiml@gmail.com>
2026-09-24 19:11:00 +0530

    Time: 0 ms (100%), Space: 12.5 MB (20.17%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,9 +1,28 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int i = 0 ; i < n  ; i++){
-            nums1[m+i] = nums2[i]; 
+       // Approach -1 Time Complexity - O(nlogn)
+        // for (int i = 0 ; i < n  ; i++){
+        //     nums1[m+i] = nums2[i]; 
+        // }
+        // sort(nums1.begin(),nums1.end());
+
+        // Approach - 2 Time Complexity - O(n)
+        int p1 = m-1 ;
+        int p2 = n-1 ;
+
+        for (int p = m + n -1 ; p >= 0 ; p-- ){
+            if (p2 < 0){
+                break;
+            }
+            if (p1 >= 0 && nums1[p1] > nums2[p2]){
+                nums1[p] = nums1[p1];
+                p1--;
+            }
+            else{
+                nums1[p] = nums2[p2];
+                p2--;
+            }
         }
-        sort(nums1.begin(),nums1.end());
     }
 };
